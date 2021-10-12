2b51a0d6b7698e2cfbca00b60e77adfb761e69ee
Jair <jairaceves56@gmail.com>
2021-10-12 17:23:15 -0500

    Trabajando las constantes

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,24 @@
+
+
+console.log('Hola Jair');
+
+// Variables y Constantes 
+const nombre = 'Jair';      // Si el valor nunca cambia
+// let appellido = 'Aceves'; // solo si el valor cambia
+const appellido = 'Aceves';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre,appellido, valorDado);
+
+if (true ){
+    const nombre = 'Peter';
+    // let valorDado = 6;  // Esta valor unicamente existe en esta seccion
+
+    console.log(nombre);
+}
+
+console.log(valorDado);
+
+
